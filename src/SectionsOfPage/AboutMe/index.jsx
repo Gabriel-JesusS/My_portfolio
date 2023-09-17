59255eb63357f2aa565c0da672b8da67f561be32
Gabriel-JesusS <jesusdesilva78@gmail.com>
2023-09-17 08:21:37 -0300

    update of section AboutMe

--- src/SectionsOfPage/AboutMe/index.jsx
+++ src/SectionsOfPage/AboutMe/index.jsx
@@ -10,7 +10,7 @@ function AboutMe() {
                 <MainTitle AboutMeTitle={"Sobre-mim"} />
             </div>
             <div className="ContainerText">
-                <AdditionalText AboutMe="Me chamo Gabriel de Jesus Silva, tenho 18 anos e estudo programação há 1 ano visando me autodesenvolver tanto nessa área como pessoa. Desde o momento em que escrevi minha primeira linha de código, fui cativado pela magia da programação. A paixão instantânea que senti naquele momento tornou-se a base do meu percurso como desenvolvedor. Ao longo do último ano, comecei a desenvolver meus projetos para obter experiencia e mergulhei de cabeça nesse mundo fascinante, explorando suas infinitas possibilidades e desafios emocionantes."
+                <AdditionalText AboutMe="Me chamo Gabriel de Jesus Silva, tenho 18 anos e estudo programação há 1 ano e sobre hardware e software visando me autodesenvolver tanto nessa área da tecnologia e como pessoa. Desde o momento em que escrevi minha primeira linha de código, fui cativado pela magia da programação. A paixão instantânea que senti naquele momento tornou-se a base do meu percurso como desenvolvedor. Ao longo do último ano, comecei a desenvolver meus projetos para obter experiencia e mergulhei de cabeça nesse mundo fascinante, explorando suas infinitas possibilidades e desafios emocionantes."
                     AboutMe2="Em resumo, minha jornada na programação tem sido emocionante e gratificante até agora. Estou animado para o que o futuro reserva e comprometido em me tornar um profissional de destaque nessa área, sempre buscando maneiras de crescer e me superar. Acredito no poder transformador da programação e estou determinado a fazer a diferença através do meu trabalho como desenvolvedor." />
             </div>
         
